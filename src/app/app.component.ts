32343f5906f42df586da87c5b0be71d2e9d1f189
vl <vlalithamech@gmail.com>
2021-05-12 12:44:10 +0530

    jhgj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getClosureSafeProperty } from '@angular/core/src/util/property';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-three';
+  content='Save Me';
+  display= false;
+  countArray=[];
+  countArrayFromFive=this.countArray.slice(5);
+  count=0;
+  displayMethod(){
+    this.display = !this.display;
+    this.count=this.count +1;
+    this.countArray.push(this.count);
+    if(this.count>=5){
+      this.countArrayFromFive.push(this.count);
+      delete this.countArray.pop();
+    }
+  }
+  getColor(){
+    return this.count>=5? 'red': 'transparent';
+  }
+  getDisplay(){
+    return this.display ?'block':'none';
+  }
 }
